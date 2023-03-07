f6ae642e4501cb3625ff09ced05d015b375f7382
canyapalak <canyapalak@gmail.com>
2023-03-07 13:27:13 +0100

    🐛 fixing bugs

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
+  // authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
